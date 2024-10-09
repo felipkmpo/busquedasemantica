b6ab2f5d5abf83c9f3db6164e7c75f5f01e8e592
felipkmpo <felipecampo828@gmail.com>
2024-10-09 18:12:08 -0500

    mejora de codigo para mostrar resultados con nueva columa descripcion y cast

--- src/busquedasemantica.py
+++ src/busquedasemantica.py
@@ -7,28 +7,16 @@ Original file is located at
     https://colab.research.google.com/drive/1X9KWT3f4E9JkOacxdK6pFZKZZTwRcEqp
 
 ## Instalando dependencias necesarias
-"""
+"""#importamos libreria pandas
 
-# Commented out IPython magic to ensure Python compatibility.
-#instalamos librerias sentence-trasnformers
-# %pip install -U sentence-transformers pandas
-#install pandas
-#install sentence-transformers
-# importamos libreria pandas
+
+#importamos libreria pandas, modulo "util" y "sentence trasnformers" incluidas dentro de las librerias sentence_transformers
+
 import pandas as pd
-
-#importamos modulo "util" y "sentence trasnformers" incluidas dentro de las librerias sentence_transformers
-#pip install sentence-transformers
-
 from sentence_transformers import SentenceTransformer
 from sentence_transformers import util
 
 """## Entendiendo el dataset"""
-
-# https://www.kaggle.com/datasets/omarhanyy/imdb-top-1000?resource=download
-# TODO: Cargar el archivo del dataset con  pandas
-#from google.colab import files
-#uploaded = files.upload()
 
 # TODO: mostrar los primeros 5 registros de dataframe
 pelis = pd.read_csv("IMDBtop1000.csv")
@@ -42,29 +30,50 @@ model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
 #Esta línea de código genera incrustaciones para las descripciones de las películas de su conjunto de datos utilizando el modelo Sentence Transformers preentrenado que cargamos anteriormente.
 embeddings = model.encode(pelis['Description'],batch_size=64,show_progress_bar=True)
 
-#Esta línea de código añade una nueva columna llamada 'embeddings' a su DataFrame pelis y almacena los embeddings
-#generados en esa columna. También convierte las incrustaciones de una matriz NumPy a una lista.
+"""Esta línea de código añade una nueva columna llamada 'embeddings' a su DataFrame pelis y almacena los embeddings
+generados en esa columna. También convierte las incrustaciones de una matriz NumPy a una lista."""
 pelis['embeddings'] = embeddings.tolist()
 
-"""## Calculando la similitud usando la métrica de similitud por coseno"""
+"""## Calculando la similitud usando la métrica de similitud por coseno, el código define una función llamada compute_similarity que calcula la similitud entre dos embeddings, utilizando la similitud del coseno."""
 
 def compute_similarity(example, query_embedding):
     embedding = example['embeddings']
     similarity = util.cos_sim(embedding, query_embedding).item()
     return similarity
 
-
-
-"""df=pelis #definimos la variable df para que dentro de esta quede incluido el dataframe pelis
-query_description = input("Enter a movie description: ")
-query_embedding = model.encode([query_description])[0]
-df['similarity'] = df.apply(lambda x: compute_similarity(x, query_embedding), axis=1)
-df = df.sort_values(by='similarity', ascending=False)
-
-df.head()['Title']
-"""
 ## Ejecuntando la búsqueda
-df = pelis
+# Creamos un nuevo dataframe vacío para almacenar los resultados de búsqueda
+df_results = pd.DataFrame()
+#creamos un ciclo while con el fin de realizar varias busquedas hasta que el usuario decida detener la busqueda
+while True:
+    query_description = input("Ingrese descripción de la película o escriba 'salir' para detener la búsqueda: ")
+    if query_description.lower() == 'salir':
+        break
+
+    query_embedding = model.encode([query_description])[0]
+    pelis['similarity'] = pelis.apply(lambda x: compute_similarity(x, query_embedding), axis=1)
+    pelis = pelis.sort_values(by='similarity', ascending=False)
+    pelis['Description_Cast'] = pelis['Description'] + ' ' + pelis['Cast']
+
+    # Seleccionamos las columnas relevantes
+    search_result = pelis[['Title', 'Description_Cast', 'similarity']].head()
+
+    # Guardamos los resultados en el nuevo dataframe
+    df_results = pd.concat([df_results, search_result])
+
+    print(search_result)
+
+
+
+# Imprimimos o guardamos el dataframe con todos los resultados
+print("Resultados acumulados:")
+print(df_results)
+print("Busqueda Finalizada.")
+
+
+
+
+"""df = pelis
 
 while True:
     query_description = input("ingrese descripcion de pelicula o escriba 'salir' para detener la busuqeda: ")
@@ -77,5 +86,5 @@ while True:
 
     print(df.head()['Title'])
 
-print("busqueda finalizada.")
+print("busqueda finalizada.")"""
 
